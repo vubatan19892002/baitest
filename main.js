9c1fbdb4ccdaef53bfde2a63b2a012979986d60e
vubatan19892002 <vubatan19892002@gmail.com>
2023-12-01 21:45:14 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+function showComponent() {
+  const element = document.querySelector(".p-instruct1");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+function showComponent1() {
+  var element = document.querySelector(".p-instruct2");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+function showComponent2() {
+  var element = document.querySelector(".p-instruct3");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+function showComponent3() {
+  var element = document.querySelector(".p-instruct4");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
